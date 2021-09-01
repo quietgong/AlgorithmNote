3af4b083da32e49150c79f8f5413df67593ae082
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2021-09-02 01:15:04 +0900

    2021.09.02

---
+++ BOJ/신나는 함수 실행.py
@@ -0,0 +1,27 @@
+import sys
+
+MAX = 21
+dp = [[[0] * MAX for _ in range(MAX)] for _ in range(MAX)]
+
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a < b < c:
+        dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+    return dp[a][b][c]
+
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+
+# 주어진 점화식으로 메모이제이션 기법을 사용, 3차원 배열 형태, 재귀구조
