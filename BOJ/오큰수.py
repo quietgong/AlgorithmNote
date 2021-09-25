fc97ecfd4f96a370319b4be66585925d5038a950
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2021-09-26 01:00:59 +0900

    2021.09.26

---
+++ BOJ/오큰수.py
@@ -0,0 +1,13 @@
+n = int(input())
+A = list(map(int, sys.stdin.readline().rstrip().split()))
+answer = [-1] * n
+stack = []
+
+
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+# 리스트의 인덱스를 활용
